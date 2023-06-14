3bcd4c9411fb97e0a557d1165e990073ee189bbe
usmanrahim <usmanrahim2000@gmail.com>
2023-06-14 17:40:38 +0500

    Adding constant folders

--- my-app/pages/index.jsx
+++ my-app/pages/index.jsx
@@ -1,12 +1,96 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Web3Modal, { providers } from "web3modal";
+import { BigNumber, Contract, utils } from "ethers";
+import {
+  NFT_CONTRACT_ABI,
+  NFT_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ABI,
+  TOKEN_CONTRACT_ADDRESS,
+} from "../constants";
 
 export default function Home() {
+  const zero = BigNumber.from(0);
   const [walletConnected, setWalletConnected] = useState(false);
+  const [tokensMinted, setTokensMinted] = useState(zero);
+  const [balanceOfCryptoDevTokens, setBalanceOfCryptoDevTokens] =
+    useState(zero);
 
-  useEffect(() => {}, []);
+  const [tokenAmount, setTokenAmount] = useState(zero);
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const { chainId } = await web3Provider.getNetwork();
+
+    if (chainId !== 5) {
+      window.alert("Change the network to goerli");
+      throw new Error("Change the network to Goerli");
+    }
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const mintCryptoDevToken = async (amount) => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+    }
+    connectWallet();
+  }, []);
+
+  const renderButton = () => {
+    return (
+      <div style={{ display: "flex-col" }}>
+        <div>
+          <input
+            type="number"
+            placeholder="Amount of tokens"
+            onChange={(e) => setTokenAmount(BigNumber.formatEther(e))}
+          />
+          <button
+            className={styles.button}
+            disabled={!(tokenAmount > 0)}
+            onClick={() => mintCryptoDevToken(tokenAmount)}
+          >
+            Mint Token
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
       <Head>
@@ -20,6 +104,24 @@ export default function Home() {
           <div className={styles.description}>
             You can claim or mint Crypto Dev tokens here
           </div>
+
+          {walletConnected ? (
+            <div>
+              <div className={styles.description}>
+                You have minted {utils.formatEther(balanceOfCryptoDevTokens)}{" "}
+                crypto devs tokens
+              </div>
+              <div className={styles.description}>
+                Overall {utils.formatEther(tokensMinted)}/10000 tokens have been
+                minted
+              </div>
+              {renderButton()}
+            </div>
+          ) : (
+            <button className={styles.button} onClick={connectWallet}>
+              Connect Wallet
+            </button>
+          )}
         </div>
       </div>
     </div>
